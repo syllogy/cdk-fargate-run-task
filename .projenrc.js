7dfd71615736cbdf7435f1cd47fb8563f7bcdff5
Pahud Hsieh <pahudnet@gmail.com>
2021-05-27 19:36:11 +0800

    chore: disable ProjenYarnUpgrade (#112)

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,5 @@
 const { AwsCdkConstructLibrary } = require('projen');
 
-const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
-
 const project = new AwsCdkConstructLibrary({
   authorAddress: 'pahudnet@gmail.com',
   authorName: 'Pahud',
@@ -29,8 +27,6 @@ const project = new AwsCdkConstructLibrary({
     module: 'cdk_fargate_run_task',
   },
   dependabot: false,
-  // upgrade every Sunday 6AM
-  projenUpgradeSchedule: ['0 6 * * 0'],
   keywords: [
     'cdk',
     'fargate',
@@ -38,49 +34,8 @@ const project = new AwsCdkConstructLibrary({
     'ecs',
     'aws',
   ],
-
 });
 
-
-// create a custom projen and yarn upgrade workflow
-const workflow = project.github.addWorkflow('ProjenYarnUpgrade');
-
-workflow.on({
-  schedule: [{
-    cron: '11 0 * * *',
-  }], // 0:11am every day
-  workflow_dispatch: {}, // allow manual triggering
-});
-
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
-});
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
 project.npmignore.exclude(...common_exclude);

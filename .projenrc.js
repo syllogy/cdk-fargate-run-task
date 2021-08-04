54265ef6bfbc6eb8f96d0319ca1ecc3c08dc7a73
Pahud Hsieh <pahudnet@gmail.com>
2021-08-04 10:26:55 +0800

    bump xmldom to 0.7.0 (#128)
    Fixes #

--- .projenrc.js
+++ .projenrc.js
@@ -48,7 +48,7 @@ const project = new AwsCdkConstructLibrary({
 });
 
 project.package.addField('resolutions', {
-  'trim-newlines': '3.0.1',
+  xmldom: 'github:xmldom/xmldom#0.7.0',
 });
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];

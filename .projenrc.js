9d241930258e244b64487f75bc7ab5d48ca7e1ef
Pahud Hsieh <pahudnet@gmail.com>
2021-08-31 17:07:29 +0800

    fix xmldom resolution hack (#156)
    Fixes #

--- .projenrc.js
+++ .projenrc.js
@@ -47,10 +47,6 @@ const project = new AwsCdkConstructLibrary({
   ],
 });
 
-project.package.addField('resolutions', {
-  xmldom: 'github:xmldom/xmldom#0.7.0',
-});
-
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
 project.npmignore.exclude(...common_exclude);
 project.gitignore.exclude(...common_exclude);
